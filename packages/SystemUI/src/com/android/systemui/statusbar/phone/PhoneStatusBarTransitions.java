4afed4f38b9119adab61bc8d90d9b3137f19220d
Allan Hedelain <tristan202@gmail.com>
2014-11-23 19:57:49 +0100

    Statusbar clock and date customizations [1/2]
    Some things changed on kitkat + our old code needed in general a bit
    rework. All done with this patchset
    
    This commit add support for:
    
    - Clock and date enable/disable
    - Clock and date position
    - AM/PM styles multilang support
    - color
    - Date format
    - Date style
    
    Originally by kufikugel. I only ported it to 5.0
    
    PS2:
    - several fixes and optimizations and code cleanup
    
    PS3:
    - more adaptions to LP
    
    PS4:
    - cleanup
    
    PS5:
    - animate hiding correctly on center clock mode
    
    Change-Id: I456bc81c0c981c04467c9908c164e7c3b6cb75d3
    
    SystemUI: Status bar clock - fixes
    
    This commit fixes:
    
    - no overlapping more from status bar icons on center clock.
      The width depending on what the user has setuped (date and clock) is exactly meassured now
    - do not start animations if they are not needed
    - reduce settings provider calls
    - a bit cleanup
    
    Forward ported to marshmallow by liquid0624
    
    Change-Id: I08892e9cd400812eae22385b6d53277f071d0240

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/PhoneStatusBarTransitions.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/PhoneStatusBarTransitions.java
@@ -33,7 +33,8 @@ public final class PhoneStatusBarTransitions extends BarTransitions {
     private final PhoneStatusBarView mView;
     private final float mIconAlphaWhenOpaque;
 
-    private View mLeftSide, mStatusIcons, mSignalCluster, mClock, mNetworkTraffic;
+    private View mLeftSide, mStatusIcons, mSignalCluster, mClock, mCenterClock, mNetworkTraffic;
+
     private Animator mCurrentAnimation;
 
     public PhoneStatusBarTransitions(PhoneStatusBarView view) {
@@ -49,6 +50,7 @@ public final class PhoneStatusBarTransitions extends BarTransitions {
         mSignalCluster = mView.findViewById(R.id.signal_cluster);
         //mBattery = mView.findViewById(R.id.battery);
         mClock = mView.findViewById(R.id.clock);
+        mCenterClock = mView.findViewById(R.id.center_clock);
         mNetworkTraffic = mView.findViewById(R.id.networkTraffic);
         applyModeBackground(-1, getMode(), false /*animate*/);
         applyMode(getMode(), false /*animate*/);
@@ -95,7 +97,8 @@ public final class PhoneStatusBarTransitions extends BarTransitions {
                     animateTransitionTo(mSignalCluster, newAlpha),
                     animateTransitionTo(mNetworkTraffic, newAlpha),
                     //animateTransitionTo(mBattery, newAlphaBC),
-                    animateTransitionTo(mClock, newAlphaBC)
+                    animateTransitionTo(mClock, newAlphaBC),
+                    animateTransitionTo(mCenterClock, newAlphaBC)
                     );
             if (isLightsOut(mode)) {
                 anims.setDuration(LIGHTS_OUT_DURATION);
@@ -109,6 +112,7 @@ public final class PhoneStatusBarTransitions extends BarTransitions {
             mNetworkTraffic.setAlpha(newAlpha);
 //            mBattery.setAlpha(newAlphaBC);
             mClock.setAlpha(newAlphaBC);
+            mCenterClock.setAlpha(newAlphaBC);
         }
     }
 }
