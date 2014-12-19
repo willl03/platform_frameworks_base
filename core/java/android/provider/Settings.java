b20e900b53a7bc7d10cf84af9e1a00a7b796a890
Dave Kessler <activethrasher00@gmail.com>
2014-12-19 18:21:01 -0500

    Power menu customizations [1/2]
    power
    reboot
    screenshot
    airplane mode
    user switcher
    bug reports
    sound modes
    assist
    voice search
    
    Beanstown106 edits:
    *adapted for android 6.0/7.0
    *added assist and voice search options

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -9089,6 +9089,12 @@ public final class Settings {
         public static final String RETAIL_DEMO_MODE_CONSTANTS = "retail_demo_mode_constants";
 
         /**
+         * String to contain power menu actions
+         * @hide
+         */
+        public static final String POWER_MENU_ACTIONS = "power_menu_actions";
+
+        /**
          * Settings to backup. This is here so that it's in the same place as the settings
          * keys and easy to update.
          *
