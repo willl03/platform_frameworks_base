ac427ad2726bb2e286df841a39354dc76ba4eb53
maxwen <max.weninger@gmail.com>
2016-02-07 23:49:56 +0100

    [1/2] base: battery styles are back
    Change-Id: I0b457e8d93af4d63c94ce0784733331d825926fe
    
    base: some battery styles need smaller font to show 100
    
    Change-Id: I3d8105c4f5a193d962415bc030afb5a2e041b8f5
    
    base: battery styles: need to check special 100% font every time
    
    Change-Id: Iaba666722f48eaf35dc1fa050e0de12b1f03290d
    
    base: adjust battery style text
    
    use same font style as the clock
    
    Change-Id: I85ba0214b0d6eaa2c9e8c3bc934be2608dd5ff2a
    
    base: tweak battery text alignment
    
    lets face it - 100% battery is something you wont see very often
    so its makes no sense to create default layout for that and
    create useless space in 99% of the time.
    
    Instead make the font smaller in that case and use shorter
    text for the default layout.
    
    [1/2] base: add battery display config
    everything else on the statusbar can be hidden
    why not also the battery if you want
    
    Change-Id: Id105fc5f950fb31bd0a346d0bed246380991719e

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/PhoneStatusBar.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/PhoneStatusBar.java
@@ -101,7 +101,9 @@ import android.view.WindowManagerGlobal;
 import android.view.animation.AccelerateInterpolator;
 import android.view.animation.Interpolator;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
 import android.widget.TextView;
+import android.widget.LinearLayout;
 import com.android.internal.logging.MetricsLogger;
 import com.android.internal.logging.MetricsProto.MetricsEvent;
 import com.android.internal.statusbar.NotificationVisibility;
@@ -127,6 +129,7 @@ import com.android.systemui.doze.DozeHost;
 import com.android.systemui.doze.DozeLog;
 import com.android.systemui.keyguard.KeyguardViewMediator;
 import com.android.systemui.qs.QSContainer;
+import com.android.systemui.omni.BatteryViewManager;
 import com.android.systemui.qs.QSPanel;
 import com.android.systemui.recents.ScreenPinningRequest;
 import com.android.systemui.recents.events.EventBus;
@@ -388,6 +391,9 @@ public class PhoneStatusBar extends BaseStatusBar implements DemoMode,
 
     int[] mAbsPos = new int[2];
     ArrayList<Runnable> mPostCollapseRunnables = new ArrayList<>();
+
+    // battery styles
+    private BatteryViewManager mBatteryViewManager;
 
     private boolean mAutomaticBrightness;
     private boolean mBrightnessControl;
@@ -902,7 +908,6 @@ public class PhoneStatusBar extends BaseStatusBar implements DemoMode,
         // set the initial view visibility
         setAreThereNotifications();
 
-        createIconController();
 
         // Background thread for any controllers that need it.
         mHandlerThread = new HandlerThread(TAG, Process.THREAD_PRIORITY_BACKGROUND);
@@ -925,6 +930,13 @@ public class PhoneStatusBar extends BaseStatusBar implements DemoMode,
                 // noop
             }
         });
+
+        // must be before StatusBarIconController
+        LinearLayout batteryContainer = (LinearLayout) mStatusBarView.findViewById(R.id.battery_container);
+        mBatteryViewManager = new BatteryViewManager(mContext, batteryContainer, mStatusBarView.getBarTransitions(), null);
+
+        createIconController();
+
         mNetworkController = new NetworkControllerImpl(mContext, mHandlerThread.getLooper());
         mNetworkController.setUserSetupComplete(mUserSetup);
         mHotspotController = new HotspotControllerImpl(mContext);
@@ -992,8 +1004,8 @@ public class PhoneStatusBar extends BaseStatusBar implements DemoMode,
         mKeyguardStatusBar.setUserSwitcherController(mUserSwitcherController);
         mUserInfoController.reloadUserInfo();
 
-        ((BatteryMeterView) mStatusBarView.findViewById(R.id.battery)).setBatteryController(
-                mBatteryController);
+        mBatteryViewManager.setBatteryController(mBatteryController);
+        mHeader.setBatteryController(mBatteryController);
         mKeyguardStatusBar.setBatteryController(mBatteryController);
 
         PowerManager pm = (PowerManager) mContext.getSystemService(Context.POWER_SERVICE);
@@ -3638,6 +3650,7 @@ public class PhoneStatusBar extends BaseStatusBar implements DemoMode,
         clearCurrentMediaNotification();
         mLockscreenWallpaper.setCurrentUser(newUserId);
         updateMediaMetaData(true, false);
+        mBatteryViewManager.update();
     }
 
     private void setControllerUsers() {
@@ -5097,5 +5110,9 @@ public class PhoneStatusBar extends BaseStatusBar implements DemoMode,
                 }
             }
         }
+    }
+
+    protected BatteryViewManager getBatteryViewManager() {
+        return mBatteryViewManager;
     }
 }
