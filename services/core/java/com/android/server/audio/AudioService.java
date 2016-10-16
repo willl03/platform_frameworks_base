00f29c4e4d0f9f876e99d5a758dad980eda7a365
willl03 <wgangers@gmail.com>
2016-10-16 16:45:29 -0400

    Tweak volume steps

--- services/core/java/com/android/server/audio/AudioService.java
+++ services/core/java/com/android/server/audio/AudioService.java
@@ -263,16 +263,16 @@ public class AudioService extends IAudioService.Stub {
 
    /** Maximum volume index values for audio streams */
     private static int[] MAX_STREAM_VOLUME = new int[] {
-        5,  // STREAM_VOICE_CALL
-        7,  // STREAM_SYSTEM
-        7,  // STREAM_RING
-        15, // STREAM_MUSIC
-        7,  // STREAM_ALARM
-        7,  // STREAM_NOTIFICATION
-        15, // STREAM_BLUETOOTH_SCO
-        7,  // STREAM_SYSTEM_ENFORCED
-        15, // STREAM_DTMF
-        15  // STREAM_TTS
+        10,  // STREAM_VOICE_CALL
+        10,  // STREAM_SYSTEM
+        10,  // STREAM_RING
+        30, // STREAM_MUSIC
+        10,  // STREAM_ALARM
+        10,  // STREAM_NOTIFICATION
+        10, // STREAM_BLUETOOTH_SCO
+        10,  // STREAM_SYSTEM_ENFORCED
+        10, // STREAM_DTMF
+        10  // STREAM_TTS
     };
 
     /** Minimum volume index values for audio streams */
