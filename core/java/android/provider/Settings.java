decee9846f1413966f818ecad0210257da46afaf
beanstown106 <nbenis106@gmail.com>
2016-10-16 22:36:57 -0400

    Custom Carrier Label & Carrier Label Placement [1/2] [Squashed]
    Squash Of:
    Keyguard/Statusbar Carrier label options [1/2]
    * Stock behavior(only on keyguard), only on statusbar,both disabled or both enabled
    * code originally writted by @Altaf-Mahdi i just squased into one commit
    https://github.com/PureNexusProject-Legacy/android_frameworks_base/commit/688d56fe23f1f7d5c3eedfd78e0ac13d32d1cb84
    
    Show carrier label / custom & change color [1/2]
    *code originally written by @martincz
    *includes various fixs by the du team
    https://github.com/PureNexusProject-Legacy/android_frameworks_base/commit/16a247ae6468127320c584b053aa095c169d01b8
    
    Carrier label - fix capitilization bug
    https://github.com/PureNexusProject-Legacy/android_frameworks_base/commit/4b9e22b6e544b5a8a4d353e7ac37453c2fdfd3c6
    
    remove carrier label color and make carrier label play nice with dark statusbar
    https://github.com/PureNexusProject-Legacy/android_frameworks_base/commit/3f3210229202b8b61bfd875e4429a26b4b267b7f
    
    Fix Carrier Label font size not changing on system font size change
    https://github.com/DirtyUnicorns/android_frameworks_base/commit/8da9dcf5d401828bd60855d6a3bb56e386926889
    
    include carrier label in icon merger width calculation
    https://github.com/PureNexusProject-Legacy/android_frameworks_base/commit/9df3a0c41fcade40e84766987896baff9f560cc7
    
    Credit/Thanks to @Altaf-Mahdi @beanstown106 @dwitherell and @martincz
    
    PureNexus Edits:
    *adapt statusbar hook for N

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -3975,6 +3975,22 @@ public final class Settings {
         public static final String SCREENSHOT_TYPE = "screenshot_type";
 
         /**
+         * custom carrier label.
+         * @hide
+         */
+        public static final String CUSTOM_CARRIER_LABEL = "custom_carrier_label";
+
+        /**
+         * Status bar carrier label
+         * 0: Hide
+         * 1: Display on keyguard status bar
+         * 2: Display on Normal status bar
+         * 3: Enabled for both
+         * @hide
+         */
+        public static final String STATUS_BAR_SHOW_CARRIER = "status_bar_show_carrier";
+
+        /**
          * Settings to backup. This is here so that it's in the same place as the settings
          * keys and easy to update.
          *
