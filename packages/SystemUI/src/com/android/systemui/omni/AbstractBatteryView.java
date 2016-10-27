5f8d4ec78df04050d20f55a1ce7e2cc35c6e90ab
maxwen <max.weninger@gmail.com>
2016-10-27 23:30:29 +0200

    base: SystemUI: update power save status correct
    Change-Id: I9c069fd5a15d318d3e3f43dcdea9cc369a6fb606

--- packages/SystemUI/src/com/android/systemui/omni/AbstractBatteryView.java
+++ packages/SystemUI/src/com/android/systemui/omni/AbstractBatteryView.java
@@ -243,6 +243,8 @@ public abstract class AbstractBatteryView extends View implements BatteryControl
 
     @Override
     public void onPowerSaveChanged(boolean isPowerSave) {
+        mPowerSaveEnabled = mBatteryController.isPowerSave();
+        postInvalidate();
     }
 
     @Override
