e551e502bbe6790cb00021b054d8f31c7e5a8bf1
dwitherell <devonwitherell1@gmail.com>
2016-10-28 00:34:23 -0600

    Update longpress back to kill logic *Made some changes in how foreground app is determined - now uses UsageStats *Edited methods so toast shows package name being killed

--- core/java/com/android/internal/util/simpleaosp/SaospUtils.java
+++ core/java/com/android/internal/util/simpleaosp/SaospUtils.java
@@ -16,22 +16,28 @@
 
 package com.android.internal.util.simpleaosp;
 
+import android.app.ActivityManager;
 import android.app.ActivityManagerNative;
 import android.app.IActivityManager;
-import android.app.ActivityManager.RunningAppProcessInfo;
+import android.app.usage.UsageStats;
+import android.app.usage.UsageStatsManager;
 import android.content.Context;
 import android.content.Intent;
 import android.content.res.Resources;
+import android.content.pm.PackageManager;
 import android.content.pm.ResolveInfo;
-import android.os.Process;
 import android.os.RemoteException;
 import android.os.UserHandle;
-import android.provider.Settings;
+import android.util.Log;
 
+import java.util.Collections;
+import java.util.Comparator;
 import java.util.List;
 import java.util.Locale;
 
 public class SaospUtils {
+
+    private static final String TAG = SaospUtils.class.getSimpleName();
 
     public static boolean isChineseLanguage() {
        return Resources.getSystem().getConfiguration().locale.getLanguage().startsWith(
@@ -43,44 +49,84 @@ public class SaospUtils {
      * This is function governed by {@link Settings.Secure.KILL_APP_LONGPRESS_BACK}.
      *
      * @param context the current context, used to retrieve the package manager.
+     * @param userId the ID of the currently active user
      * @return {@code true} when a user application was found and closed.
      */
-    public static boolean killForegroundApplication(Context context) {
-        boolean targetKilled = false;
+    public static String killForegroundApp(Context context, int userId) {
         try {
+            return killForegroundAppInternal(context, userId);
+        } catch (RemoteException e) {
+            Log.e(TAG, "Could not kill foreground app");
+        }
+        return null;
+    }
+
+    private static String killForegroundAppInternal(Context context, int userId)
+            throws RemoteException {
+        try {
+            PackageManager packageManager = context.getPackageManager();
             final Intent intent = new Intent(Intent.ACTION_MAIN);
             String defaultHomePackage = "com.android.launcher";
             intent.addCategory(Intent.CATEGORY_HOME);
-            final ResolveInfo res = context.getPackageManager().resolveActivity(intent, 0);
+            final ResolveInfo res = packageManager.resolveActivity(intent, 0);
+
             if (res.activityInfo != null && !res.activityInfo.packageName.equals("android")) {
                 defaultHomePackage = res.activityInfo.packageName;
             }
-            IActivityManager am = ActivityManagerNative.getDefault();
-            List<RunningAppProcessInfo> apps = am.getRunningAppProcesses();
-            for (RunningAppProcessInfo appInfo : apps) {
-                int uid = appInfo.uid;
-                // Make sure it's a foreground user application (not system,
-                // root, phone, etc.)
-                if (uid >= Process.FIRST_APPLICATION_UID && uid <= Process.LAST_APPLICATION_UID
-                        && appInfo.importance == RunningAppProcessInfo.IMPORTANCE_FOREGROUND) {
-                    if (appInfo.pkgList != null && (appInfo.pkgList.length > 0)) {
-                        for (String pkg : appInfo.pkgList) {
-                            if (!pkg.equals("com.android.systemui") && !pkg.equals(defaultHomePackage)) {
-                                am.forceStopPackage(pkg, UserHandle.USER_CURRENT);
-                                targetKilled = true;
-                                break;
-                            }
-                        }
-                    } else {
-                        Process.killProcess(appInfo.pid);
-                        targetKilled = true;
-                        break;
+            // Use UsageStats to determine foreground app
+            UsageStatsManager usageStatsManager = (UsageStatsManager)
+                context.getSystemService(Context.USAGE_STATS_SERVICE);
+            long current = System.currentTimeMillis();
+            long past = current - (1000 * 60 * 60); // uses snapshot of usage over past 60 minutes
+
+            // Get the list, then sort it chronilogically so most recent usage is at start of list
+            List<UsageStats> recentApps = usageStatsManager.queryUsageStats(
+                UsageStatsManager.INTERVAL_DAILY, past, current);
+            Collections.sort(recentApps, new Comparator<UsageStats>() {
+                @Override
+                public int compare(UsageStats lhs, UsageStats rhs) {
+                    long timeLHS = lhs.getLastTimeUsed();
+                    long timeRHS = rhs.getLastTimeUsed();
+                    if (timeLHS > timeRHS) {
+                        return -1;
+                    } else if (timeLHS < timeRHS) {
+                        return 1;
                     }
+                    return 0;
                 }
+            });
+
+            IActivityManager am = ActivityManagerNative.getDefault();
+            if (am.getLockTaskModeState() != ActivityManager.LOCK_TASK_MODE_NONE) return null;
+            // Look for most recent usagestat with lastevent == 1 and grab package name
+            // ...this seems to map to the UsageEvents.Event.MOVE_TO_FOREGROUND
+            String pkg = null;
+            for (int i = 0; i < recentApps.size(); i++) {
+                UsageStats mostRecent = recentApps.get(i);
+                if (mostRecent.mLastEvent == 1) {
+                    pkg = mostRecent.mPackageName;
+                    break;
+                }
+            }
+
+            if (pkg != null && !pkg.equals("com.android.systemui")
+                    && !pkg.equals(defaultHomePackage)) {
+                am.forceStopPackage(pkg, UserHandle.USER_CURRENT);
+                try {
+                    String appName = (String) packageManager.getApplicationLabel(
+                        packageManager.getApplicationInfo(pkg, PackageManager.GET_META_DATA));
+                    return appName;
+                } catch (PackageManager.NameNotFoundException e) {
+                    // Just use pkg if issues getting appName
+                    return pkg;
+                }
+            } else {
+                return null;
             }
         } catch (RemoteException remoteException) {
             // Do nothing; just let it go.
         }
-        return targetKilled;
+        return null;
     }
 }
+
