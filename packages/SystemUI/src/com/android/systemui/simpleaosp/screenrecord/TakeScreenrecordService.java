PureNexusProject-Legacy/android_frameworks_base

Forward Port ScreenRecorder from 4.4 (Squashed)
Squashed commit of the following:

Author: Evisceration <eviscerationls@gmail.com>
Date:   Fri Mar 14 17:30:00 2014 -0700
Add a timer to screen record
Change-Id: I0851344670ef2e55c044c255daf907d6e72614e0

Author: Alexander Martinz <eviscerationls@gmail.com>
Date:   Tue Jan 19 21:23:57 2016 +0100
Save screenrecord in "Screenrecords" directory
This was previously saved in "Screenshots"
- Also refactor source a bit, remove warnings
PS2: LOG screenshots --> screenrecords
Change-Id: Idf9b024bbbf1816aa91e5e455525750a761ddb73

Author: maxwen <max.weninger@gmail.com>
Date:   Sat Nov 28 15:38:42 2015 +0100
screen record: make sure to always delete tmp files
Tell media scanner that the tmp file got deleted
Change-Id: I2974969fa25a01c9422f13316d188b65db591156

Author: maxwen <max.weninger@gmail.com>
Date:   Sun Dec 28 21:31:46 2014 +0100
base: screenrecord: use different notification image
Change-Id: I252144a4f5c3bdaa84974b996348885ba8dee942

Author: XpLoDWilD <xplod@ouverta.fr>
Date:   Sun Jul 13 21:43:28 2014 +0200
Screenrecord: Fix SystemUI loop-crash
Change-Id: I25045820465d21b7d1f0e933e5e4f3324cb044b6

Author: maxwen <max.weninger@gmail.com>
Date:   Tue Apr 29 23:28:06 2014 +0200
base: screenrecord: update notification
Update show/hide pointer button for actual state
Change-Id: I02090f05b06896248483f3f9ff2eb22924a22476

Author: maxwen <max.weninger@gmail.com
Date:   Tue Dec 23 01:04:35 2014 +0100
Screenrecord: correct multi-user acces for settings
Change-Id: I0bee907d190b2dc0df1c12d136f31ed379a59866

Author: xplodwild <me@xplod.fr>
Date:   Wed Nov 13 08:16:57 2013 +0100
Screenrecord: Create the Screenshots directory if it doesn't exist
Change-Id: I2b0458eed024dbda97e9e1ea3426c12d29add781

Author: xplodwild <me@xplod.fr>
Date:   Tue Nov 5 20:30:09 2013 +0100
base: Add a key combo to start/stop video recording
Press volume up and power to start recording a video of your screen.
Press this key combo again to stop it.
Change-Id:

packages/SystemUI/src/com/android/systemui/purenexus/screenrecord/TakeScreenrecordService.java
@@ -1,0 +1,118 @@
+/*
+ * Copyright (C) 2011 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * Per article 5 of the Apache 2.0 License, some modifications to this code
+ * were made by the OmniROM Project.
+ *
+ * Modifications Copyright (C) 2013 The OmniROM Project
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 3
+ * of the License, or (at your option) any later version.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ */
+
+package com.android.systemui.purenexus.screenrecord;
+
+import android.app.Service;
+import android.content.Intent;
+import android.os.Handler;
+import android.os.IBinder;
+import android.os.Message;
+import android.os.Messenger;
+import android.os.RemoteException;
+import android.os.UserHandle;
+import android.provider.Settings;
+
+public class TakeScreenrecordService extends Service {
+    private static final String TAG = "TakeScreenrecordService";
+
+    public static final String ACTION_START = "start";
+    public static final String ACTION_STOP = "stop";
+    public static final String ACTION_TOGGLE_POINTER = "toggle_pointer";
+
+    private static GlobalScreenrecord mScreenrecord;
+
+    private Handler mHandler = new Handler() {
+        @Override
+        public void handleMessage(Message msg) {
+            switch (msg.what) {
+                case 1:
+                    final Messenger callback = msg.replyTo;
+                    toggleScreenrecord();
+
+                    Message reply = Message.obtain(null, 1);
+                    try {
+                        callback.send(reply);
+                    } catch (RemoteException e) {
+                    }
+            }
+        }
+    };
+
+    @Override
+    public IBinder onBind(Intent intent) {
+        return new Messenger(mHandler).getBinder();
+    }
+
+    @Override
+    public int onStartCommand(Intent intent, int flags, int startId) {
+        if (intent != null) {
+            if (intent.getAction().equals(ACTION_START)) {
+                startScreenrecord();
+            } else if (intent.getAction().equals(ACTION_STOP)) {
+                stopScreenrecord();
+            } else if (intent.getAction().equals(ACTION_TOGGLE_POINTER)) {
+                int currentStatus = Settings.System.getIntForUser(getContentResolver(),
+                            Settings.System.SHOW_TOUCHES, 0, UserHandle.USER_CURRENT);
+                Settings.System.putIntForUser(getContentResolver(), Settings.System.SHOW_TOUCHES,
+                            1 - currentStatus, UserHandle.USER_CURRENT);
+                mScreenrecord.updateNotification();
+            }
+        }
+
+        return super.onStartCommand(intent, flags, startId);
+    }
+
+    private void startScreenrecord() {
+        if (mScreenrecord == null) {
+            mScreenrecord = new GlobalScreenrecord(TakeScreenrecordService.this);
+        }
+        mScreenrecord.takeScreenrecord();
+    }
+
+    private void stopScreenrecord() {
+        if (mScreenrecord == null) {
+            return;
+        }
+        mScreenrecord.stopScreenrecord();
+
+        // Turn off pointer in all cases
+        Settings.System.putIntForUser(getContentResolver(), Settings.System.SHOW_TOUCHES,
+                0, UserHandle.USER_CURRENT);
+    }
+
+    private void toggleScreenrecord() {
+        if (mScreenrecord == null || !mScreenrecord.isRecording()) {
+            startScreenrecord();
+        } else {
+            stopScreenrecord();
+        }
+    }
+}

willl03/platform_frameworks_base

packages/SystemUI/src/com/android/systemui/simpleaosp/screenrecord/TakeScreenrecordService.java
@@ -1,0 +1,118 @@
+/*
+ * Copyright (C) 2011 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * Per article 5 of the Apache 2.0 License, some modifications to this code
+ * were made by the OmniROM Project.
+ *
+ * Modifications Copyright (C) 2013 The OmniROM Project
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 3
+ * of the License, or (at your option) any later version.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ */
+
+package com.android.systemui.simpleaosp.screenrecord;
+
+import android.app.Service;
+import android.content.Intent;
+import android.os.Handler;
+import android.os.IBinder;
+import android.os.Message;
+import android.os.Messenger;
+import android.os.RemoteException;
+import android.os.UserHandle;
+import android.provider.Settings;
+
+public class TakeScreenrecordService extends Service {
+    private static final String TAG = "TakeScreenrecordService";
+
+    public static final String ACTION_START = "start";
+    public static final String ACTION_STOP = "stop";
+    public static final String ACTION_TOGGLE_POINTER = "toggle_pointer";
+
+    private static GlobalScreenrecord mScreenrecord;
+
+    private Handler mHandler = new Handler() {
+        @Override
+        public void handleMessage(Message msg) {
+            switch (msg.what) {
+                case 1:
+                    final Messenger callback = msg.replyTo;
+                    toggleScreenrecord();
+
+                    Message reply = Message.obtain(null, 1);
+                    try {
+                        callback.send(reply);
+                    } catch (RemoteException e) {
+                    }
+            }
+        }
+    };
+
+    @Override
+    public IBinder onBind(Intent intent) {
+        return new Messenger(mHandler).getBinder();
+    }
+
+    @Override
+    public int onStartCommand(Intent intent, int flags, int startId) {
+        if (intent != null) {
+            if (intent.getAction().equals(ACTION_START)) {
+                startScreenrecord();
+            } else if (intent.getAction().equals(ACTION_STOP)) {
+                stopScreenrecord();
+            } else if (intent.getAction().equals(ACTION_TOGGLE_POINTER)) {
+                int currentStatus = Settings.System.getIntForUser(getContentResolver(),
+                            Settings.System.SHOW_TOUCHES, 0, UserHandle.USER_CURRENT);
+                Settings.System.putIntForUser(getContentResolver(), Settings.System.SHOW_TOUCHES,
+                            1 - currentStatus, UserHandle.USER_CURRENT);
+                mScreenrecord.updateNotification();
+            }
+        }
+
+        return super.onStartCommand(intent, flags, startId);
+    }
+
+    private void startScreenrecord() {
+        if (mScreenrecord == null) {
+            mScreenrecord = new GlobalScreenrecord(TakeScreenrecordService.this);
+        }
+        mScreenrecord.takeScreenrecord();
+    }
+
+    private void stopScreenrecord() {
+        if (mScreenrecord == null) {
+            return;
+        }
+        mScreenrecord.stopScreenrecord();
+
+        // Turn off pointer in all cases
+        Settings.System.putIntForUser(getContentResolver(), Settings.System.SHOW_TOUCHES,
+                0, UserHandle.USER_CURRENT);
+    }
+
+    private void toggleScreenrecord() {
+        if (mScreenrecord == null || !mScreenrecord.isRecording()) {
+            startScreenrecord();
+        } else {
+            stopScreenrecord();
+        }
+    }
+}