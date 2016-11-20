b6cdd77dbc91f633e070d6cfdb16c3bf5586b81e
Yasir <saeedyasir91@gmail.com>
2016-11-20 02:21:54 -0500

    Remove Omni rom battery styles [1/2]
    This reverts commit 0da2ca91c807d065448359a3fb1b7ef3d644bcbd.
    This reverts commit e8dc352075a7eeb31413171ee19e733e3dd18606.
    This reverts commit c037a71ff3eda5fba295b5c927f15a60dd48d578.
    This reverts commit 56eb65d631bd69502b92d111558ef565e9803334.
    This reverts commit f15855fe4fcdbdd76ac01138966f5353b6a69272.
    This reverts commit 5f8d4ec78df04050d20f55a1ce7e2cc35c6e90ab.

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/PhoneStatusBar.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/PhoneStatusBar.java
@@ -121,8 +121,8 @@ import android.view.WindowManagerGlobal;
 import android.view.animation.AccelerateInterpolator;
 import android.view.animation.Interpolator;
 import android.widget.ImageView;
-import android.widget.LinearLayout;
 import android.widget.TextView;
+
 import com.android.internal.logging.MetricsLogger;
 import com.android.internal.logging.MetricsProto.MetricsEvent;
 import com.android.internal.statusbar.NotificationVisibility;
@@ -147,7 +147,6 @@ import com.android.systemui.doze.DozeHost;
 import com.android.systemui.doze.DozeLog;
 import com.android.systemui.keyguard.KeyguardViewMediator;
 import com.android.systemui.qs.QSContainer;
-import com.android.systemui.omni.BatteryViewManager;
 import com.android.systemui.qs.QSPanel;
 import com.android.systemui.recents.ScreenPinningRequest;
 import com.android.systemui.recents.events.EventBus;
@@ -399,9 +398,6 @@ public class PhoneStatusBar extends BaseStatusBar implements DemoMode,
 
     int[] mAbsPos = new int[2];
     ArrayList<Runnable> mPostCollapseRunnables = new ArrayList<>();
-
-    // battery styles
-    private BatteryViewManager mBatteryViewManager;
 
     private boolean mAutomaticBrightness;
     private boolean mBrightnessControl;
@@ -924,6 +920,7 @@ public class PhoneStatusBar extends BaseStatusBar implements DemoMode,
         // set the initial view visibility
         setAreThereNotifications();
 
+        createIconController();
 
         // Background thread for any controllers that need it.
         mHandlerThread = new HandlerThread(TAG, Process.THREAD_PRIORITY_BACKGROUND);
@@ -946,13 +943,6 @@ public class PhoneStatusBar extends BaseStatusBar implements DemoMode,
                 // noop
             }
         });
-
-        // must be before StatusBarIconController
-        LinearLayout batteryContainer = (LinearLayout) mStatusBarView.findViewById(R.id.battery_container);
-        mBatteryViewManager = new BatteryViewManager(mContext, batteryContainer, mStatusBarView.getBarTransitions(), null);
-
-        createIconController();
-
         mNetworkController = new NetworkControllerImpl(mContext, mHandlerThread.getLooper());
         mNetworkController.setUserSetupComplete(mUserSetup);
         mHotspotController = new HotspotControllerImpl(mContext);
@@ -1020,8 +1010,8 @@ public class PhoneStatusBar extends BaseStatusBar implements DemoMode,
         mKeyguardStatusBar.setUserSwitcherController(mUserSwitcherController);
         mUserInfoController.reloadUserInfo();
 
-        mBatteryViewManager.setBatteryController(mBatteryController);
-        mHeader.setBatteryController(mBatteryController);
+        ((BatteryMeterView) mStatusBarView.findViewById(R.id.battery)).setBatteryController(
+                mBatteryController);
         mKeyguardStatusBar.setBatteryController(mBatteryController);
 
         mReportRejectedTouch = mStatusBarWindow.findViewById(R.id.report_rejected_touch);
@@ -3818,7 +3808,6 @@ public class PhoneStatusBar extends BaseStatusBar implements DemoMode,
         mLockscreenWallpaper.setCurrentUser(newUserId);
         mScrimController.setCurrentUser(newUserId);
         updateMediaMetaData(true, false);
-        mBatteryViewManager.update();
     }
 
     private void setControllerUsers() {
@@ -5290,9 +5279,5 @@ public class PhoneStatusBar extends BaseStatusBar implements DemoMode,
                 }
             }
         }
-    }
-
-    protected BatteryViewManager getBatteryViewManager() {
-        return mBatteryViewManager;
     }
 }
