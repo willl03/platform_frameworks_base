f06c3daf097514a5983a49fde9f47581912c0c39
Bangbang Huang <huangbangbang@huawei.com>
2016-11-24 22:49:27 +0800

    Add new feature of One-hand UI Mode Bug: 33118157 Test: builds, and test feature in bug 33118157
    Change-Id: Ic1b62a8d0821385cfe05818c72f9554588457a65

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/NavigationBarView.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/NavigationBarView.java
@@ -54,7 +54,7 @@ import com.android.systemui.RecentsComponent;
 import com.android.systemui.stackdivider.Divider;
 import com.android.systemui.statusbar.policy.BackButtonDrawable;
 import com.android.systemui.statusbar.policy.DeadZone;
-import com.android.systemui.tuner.TunerService;
+import com.android.systemui.singlehandmode.SlideTouchEvent;
 
 import java.io.FileDescriptor;
 import java.io.PrintWriter;
@@ -112,6 +112,7 @@ public class NavigationBarView extends LinearLayout {
 
     private final SparseArray<ButtonDispatcher> mButtonDisatchers = new SparseArray<>();
     private Configuration mConfiguration;
+    private SlideTouchEvent mSlideTouchEvent;
 
     private NavigationBarInflaterView mNavigationInflaterView;
 
@@ -202,7 +203,7 @@ public class NavigationBarView extends LinearLayout {
         mVertical = false;
         mShowMenu = false;
         mGestureHelper = new NavigationBarGestureHelper(context);
-
+        mSlideTouchEvent = new SlideTouchEvent(context);
         mConfiguration = new Configuration();
         mConfiguration.updateFrom(context.getResources().getConfiguration());
         updateIcons(context, Configuration.EMPTY, mConfiguration);
@@ -235,6 +236,7 @@ public class NavigationBarView extends LinearLayout {
 
     @Override
     public boolean onTouchEvent(MotionEvent event) {
+        mSlideTouchEvent.handleTouchEvent(event);
         if (mGestureHelper.onTouchEvent(event)) {
             return true;
         }
@@ -246,6 +248,7 @@ public class NavigationBarView extends LinearLayout {
 
     @Override
     public boolean onInterceptTouchEvent(MotionEvent event) {
+        mSlideTouchEvent.handleTouchEvent(event);
         return mGestureHelper.onInterceptTouchEvent(event);
     }
 
