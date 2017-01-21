08d7149fcc8e49d35312a262d5b5d993e30fa84d
snak3ater <saeedyasir91@gmail.com>
2017-01-21 09:48:56 -0500

    Frameworks: Remove Lockscreen PIN quick unlock [1/2]
    This reverts commit d34f58755300ce406680fb9ccf7f3f9a95fe8ca1.

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -6686,12 +6686,6 @@ public final class Settings {
         public static final String ASSIST_DISCLOSURE_ENABLED = "assist_disclosure_enabled";
 
         /**
-         * Whether to use the custom quick unlock screen control
-         * @hide
-         */
-        public static final String LOCKSCREEN_QUICK_UNLOCK_CONTROL = "lockscreen_quick_unlock_control";
-
-        /**
          * Names of the service components that the current user has explicitly allowed to
          * see all of the user's notifications, separated by ':'.
          *
@@ -7121,10 +7115,9 @@ public final class Settings {
             DOZE_ENABLED,
             DOZE_PULSE_ON_PICK_UP,
             DOZE_PULSE_ON_DOUBLE_TAP,
-            STATUS_BAR_LOCKED_ON_SECURE_KEYGUARD,
-            LOCKSCREEN_QUICK_UNLOCK_CONTROL,
             STATUS_BAR_SHOW_BATTERY_PERCENT,
-            STATUS_BAR_BATTERY_STYLE
+            STATUS_BAR_BATTERY_STYLE,
+            STATUS_BAR_LOCKED_ON_SECURE_KEYGUARD
         };
 
         /**
