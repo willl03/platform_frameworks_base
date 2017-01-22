df3b5d45636a27038a3cd84f68b78c9630ae96e2
beanstown106 <nbenis106@gmail.com>
2017-01-22 13:36:39 -0500

    Fingerprint authentication vibration [1/2]
    had a few requests for this, as most devices only vibrate when fingerprint authentication is unsucesful so made it configurable

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -4015,6 +4015,12 @@ public final class Settings {
         public static final String LONG_PRESS_KILL_DELAY = "long_press_kill_delay";
 
         /**
+         * whether to enable or disable vibration on succesful fingerprint auth
+         * @hide
+         */
+        public static final String FINGERPRINT_SUCCESS_VIB = "fingerprint_success_vib";
+
+        /**
          * Settings to backup. This is here so that it's in the same place as the settings
          * keys and easy to update.
          *
